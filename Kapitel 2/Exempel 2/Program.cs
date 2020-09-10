fe02009f56385e916674fe7d78fccae3bd2c1b76
TheodorBjorkman <70270703+TheodorBjorkman@users.noreply.github.com>
2020-09-10 11:14:44 +0200

    Lektionsuppgifter

---
+++ Kapitel 2/Exempel 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exempel_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            int age;
+            float height;
+            string message = "";
+
+            Console.Write("Vad heter du? ");
+            name = Console.ReadLine();
+
+            Console.Write("Hur gammal är du? ");
+            age = int.Parse(Console.ReadLine());
+
+            Console.Write("Hur lång är du (meter)? ");
+            height = float.Parse(Console.ReadLine());
+
+            message = "Hej, " + name + "!" + " Du är " + age + " år gammal och " + height + " lång.";
+
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Kapitel 2/Uppgift 2-1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Uppgift_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float height;
+            float less;
+            float worldRecord = 2.45F;
+
+            Console.Write("Hur högt kan du hoppa (m)? ");
+            height = float.Parse(Console.ReadLine());
+
+less = worldRecord - height - 0.00000005F;
+
+            if (less > 0) {
+
+                Console.WriteLine("Du hoppar " + less + " meter mindre än världsrekordet");
+                
+            } else {
+
+                Console.WriteLine("Du hoppar högre än världsrekordet!");
+
+            }
+            
+        }
+    }
+}
