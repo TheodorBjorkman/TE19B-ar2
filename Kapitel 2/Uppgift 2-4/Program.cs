2e35c0254d5f22d34e5e8a2bf7f0f19e57fd3c84
TheodorBjorkman <70270703+TheodorBjorkman@users.noreply.github.com>
2020-09-16 19:19:31 +0200

    r

---
+++ Kapitel 2/Uppgift 2-4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uppgift_2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Ange tal1: ");
+            int tal1 = int.Parse(args[0]);
+
+            //Console.Write("Ange tal2: ");
+            int tal2 = int.Parse(args[1]);
+
+            int summa = tal1 + tal2;
+            int produkt = tal1 * tal2;
+
+            Console.WriteLine($"Summan är {summa} \nprodukten är {produkt}.");
+        }
+    }
+}
