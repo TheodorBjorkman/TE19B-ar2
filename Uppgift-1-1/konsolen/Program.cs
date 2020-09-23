b907e88162475d4b826a17f5094649847ae44ff0
TheodorBjorkman <theodor.bjorkman@elev.ga.ntig.se>
2020-09-23 13:36:01 +0200

    l

---
+++ Uppgift-1-1/konsolen/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace konsolen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+    }
+}
---
+++ kap2/exempel/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace exempel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string namn = "Theodor";
+            Console.WriteLine("Hello " + namn);
+        }
+    }
+}
