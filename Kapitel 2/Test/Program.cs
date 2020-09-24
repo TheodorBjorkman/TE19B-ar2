7355a77f51e7b8aa1b69c55634fd701052f9141b
TheodorBjorkman <70270703+TheodorBjorkman@users.noreply.github.com>
2020-09-24 09:49:16 +0200

    l

---
+++ Kapitel 2/Test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv din email: ");
+            string email = Console.ReadLine();
+
+            string protocol = email.Substring(0, email.IndexOf(":") + 3);
+            Console.WriteLine(protocol);
+
+            string subdomain = email.Substring(email.IndexOf(":") + 3, email.IndexOf("." + 1));
+            Console.WriteLine(subdomain);
+
+            string domain = email.Substring(email.IndexOf(".") + 1, email.IndexOf(".") + 1);
+            Console.WriteLine(domain);
+
+            string topLevelDomain = email.Substring(email.IndexOf(".", email.IndexOf("." + 1)) + 3, email.IndexOf("/" + 1));
+            Console.WriteLine(topLevelDomain);
+
+            string path = email.Substring(email.IndexOf("/") + 1);
+            Console.WriteLine(path);
+        }
+    }
+}
