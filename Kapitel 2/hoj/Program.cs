07e9624aaf95ffc121027b3d1933f37533a93e04
TheodorBjorkman <70270703+TheodorBjorkman@users.noreply.github.com>
2020-09-24 11:01:51 +0200

    Start of Projekt1

---
+++ Kapitel 2/hoj/Program.cs
@@ -0,0 +1,26 @@
+﻿//Skapa ett program där användaren ska skriva in en beräkning innehållande två decimaltal och ett gångertecken på samma rad, t.ex. 5,2*3,8. Programmet ska utföra beräkningen och berätta vad svaret är.
+using System;
+
+namespace hoj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv multiplikation med decimal (använd komma): ");
+            string input = Console.ReadLine();
+
+            string number1 = input.Substring(0, input.IndexOf("*"));
+
+            double full1 = double.Parse(number1);
+
+            string number2 = input.Substring(input.IndexOf("*") + 1);
+
+            double full2 = double.Parse(number2);
+
+            double answer = full1 * full2;
+            
+            System.Console.WriteLine(answer);
+        }
+    }
+}
---
+++ Kapitel 3/projekt1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace projekt1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string input = Console.ReadLine();
+            input = input.ToLower();
+            if(input == "outhouse") {
+
+
+            } else if(input == "cave") {
+
+
+            }
+        }
+
+        static void Write(bool line, string input) {
+            if(line == false) {
+                Console.Write(input);
+            } else {
+                System.Console.WriteLine(input);
+            }
+        }
+    }
+}
