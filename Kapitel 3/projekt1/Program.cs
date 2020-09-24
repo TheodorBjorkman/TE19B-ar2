006a31065de428e38ee5c9e12b9167298ed248ee
TheodorBjorkman <70270703+TheodorBjorkman@users.noreply.github.com>
2020-09-24 11:06:46 +0200

    Projekt1 update 1

--- Kapitel 3/projekt1/Program.cs
+++ Kapitel 3/projekt1/Program.cs
@@ -6,23 +6,33 @@ namespace projekt1
     {
         static void Main(string[] args)
         {
-            
+            Write(false, "You are standing in front of a cave entrance located at the foot of a mountain. There is an outhouse a few meters to the left of the entrance. Where do you go? ");
+
             string input = Console.ReadLine();
+
             input = input.ToLower();
+
             if(input == "outhouse") {
 
+                Write(true, "Outhouse");
 
             } else if(input == "cave") {
 
+                Write(true, "Cave");
 
             }
         }
 
         static void Write(bool line, string input) {
+
             if(line == false) {
+
                 Console.Write(input);
+
             } else {
+
                 System.Console.WriteLine(input);
+
             }
         }
     }
