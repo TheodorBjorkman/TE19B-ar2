dd2192bac8984bd283b965fee87ae7653a043cc1
TheodorBjorkman <theodor.bjorkman@elev.ga.ntig.se>
2020-09-25 10:49:45 +0200

    Reset of test
    Preparing for creation and testing of new option selection system

--- Kapitel 3/Test/Program.cs
+++ Kapitel 3/Test/Program.cs
@@ -6,17 +6,7 @@ namespace Test
     {
         static void Main()
         {
-            int x = int.Parse(Console.ReadLine());
-            int y = int.Parse(Console.ReadLine());
-            for (x = x; x >= y; x = x - 1)
-            {
-                lmao();
-            }
-        }
 
-        static void lmao()
-        {
-            System.Console.WriteLine("XD");
         }
     }
 }
