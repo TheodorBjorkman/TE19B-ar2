3199f6ad86e5697abfa3bcfa311892775ab94ee2
TheodorBjorkman <theodor.bjorkman@elev.ga.ntig.se>
2020-09-25 09:28:35 +0200

    test stuff

---
+++ Kapitel 2/Uppgift/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string namn = "Rodoeht";
+            Console.WriteLine($"Cheeki breki? Belchi veki. {namn}");
+
+
+        }
+    }
+}
