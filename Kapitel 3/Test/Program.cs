86322fbd524475ae16da3139f20122e814d86d68
TheodorBjorkman <theodor.bjorkman@elev.ga.ntig.se>
2020-09-30 14:34:56 +0200

    Preparing menu integration

--- Kapitel 3/Test/Program.cs
+++ Kapitel 3/Test/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Test
 {
@@ -6,7 +7,104 @@ namespace Test
     {
         static void Main()
         {
+            TBA.Selector selector = new TBA.Selector();
+            selector.Add("test");
+            selector.Add("test2");
+            selector.Add("test3");
+            int output = selector.Run();
+            selector.Clear();
+            System.Console.WriteLine(output);
+        }
+    }
+}
 
+namespace TBA
+{
+    class Selector
+    {
+        int cursorPos = 0;
+        List<string> options;
+
+        public Selector()
+        {
+            options = new List<string>();
+        }
+
+        public void Clear()
+        {
+            options = new List<string>();
+        }
+
+        public void Add(string item)
+        {
+            if (!options.Contains(item))
+            {
+                options.Add(item);
+            }
+        }
+
+        public void AddRange(string[] items)
+        {
+            options.AddRange(items);
+        }
+
+        private void moveCursorPos(int amount, int min, int max)
+        {
+            cursorPos = cursorPos + amount;
+            if (cursorPos >= max)
+            {
+                cursorPos = max - 1;
+            }
+            if (cursorPos < min)
+            {
+                cursorPos = min;
+            }
+        }
+
+        void drawAtPos(string text, int pos)
+        {
+            Console.SetCursorPosition(0, pos);
+            Console.Write(text);
+        }
+
+        public int Run()
+        {
+            string[] selectOptions = options.ToArray();
+            int intialTop = Console.CursorTop;
+
+            string printString = "\n";
+            foreach (string option in selectOptions)
+            {
+                printString += "  " + option + "\n";
+            }
+            Console.WriteLine(printString);
+            drawAtPos(">", intialTop + cursorPos + 1);
+            while (true)
+            {
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        drawAtPos(" ", intialTop + cursorPos + 1);
+                        moveCursorPos(1, 0, selectOptions.Length);
+                        drawAtPos(">", intialTop + cursorPos + 1);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        drawAtPos(" ", intialTop + cursorPos + 1);
+                        moveCursorPos(-1, 0, selectOptions.Length);
+                        drawAtPos(">", intialTop + cursorPos + 1);
+                        break;
+                    case ConsoleKey.Enter:
+                        int newCursor = intialTop;
+                        for (int i = 0; i <= selectOptions.Length; i++)
+                        {
+                            newCursor++;
+                            Console.SetCursorPosition(0, newCursor);
+                            Console.Write(new string(' ', Console.WindowWidth));
+                        }
+                        Console.SetCursorPosition(0, intialTop);
+                        return cursorPos;
+                }
+            }
         }
     }
 }
--- Kapitel 3/projekt1/Program.cs
+++ Kapitel 3/projekt1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 //highlight answers
 namespace projekt1
@@ -523,6 +524,139 @@ namespace projekt1
         static void Action()
         {
             System.Console.WriteLine("Helo");
+        }
+    }
+}
+/*
+namespace Test
+{
+    class Program
+    {
+        static void Main()
+        {
+            TBA.Selector selector = new TBA.Selector();
+            selector.Add("test");
+            selector.Add("test2");
+            selector.Add("test3");
+            int output = selector.Run();
+            selector.Clear();
+            System.Console.WriteLine(output);
+        }
+    }
+}
+*/
+namespace TBA //Big brain kod från klasskompis. Har studerat tillräckligt för att förstå hur den fungerar, att skriva på egen hand skulle dock ta lång tid, mycket google och inte varit lika snyggt.
+{
+    class Selector
+    {
+        int cursorPos = 0;
+        List<string> options;
+
+        public Selector()
+        {
+            options = new List<string>();
+        }
+
+        public void Clear()
+        {
+            options = new List<string>();
+        }
+
+        public void Add(string item)
+        {
+            if (!options.Contains(item))
+            {
+                options.Add(item);
+            }
+        }
+
+        public void AddRange(string[] items)
+        {
+            options.AddRange(items);
+        }
+
+        private void moveCursorPos(int amount, int min, int max)
+        {
+            cursorPos = cursorPos + amount;
+            if (cursorPos >= max)
+            {
+                cursorPos = max - 1;
+            }
+            if (cursorPos < min)
+            {
+                cursorPos = min;
+            }
+        }
+
+        void drawAtPos(string text, int pos)
+        {
+            Console.SetCursorPosition(0, pos);
+            Console.Write(text);
+        }
+        static void Write(bool line, string write) //cool writing system
+        {
+
+            if (line == false)
+            {
+                int i;
+                for (i = 0; i <= write.Length - 1; i++)
+                {
+                    Thread.Sleep(20);
+                    Console.Write(write.Substring(i, 1));
+                }
+
+            }
+            else
+            {
+                int i;
+                for (i = 0; i <= write.Length - 1; i++)
+                {
+                    Thread.Sleep(20);
+                    Console.Write(write.Substring(i, 1));
+                }
+                System.Console.WriteLine();
+
+            }
+        }
+
+        public int Run()
+        {
+            string[] selectOptions = options.ToArray();
+            int intialTop = Console.CursorTop;
+
+            string printString = "\n";
+            foreach (string option in selectOptions)
+            {
+                printString += "  " + option + "\n";
+            }
+            Write(true, printString);
+            drawAtPos(">", intialTop + cursorPos + 1);
+            while (true)
+            {
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        drawAtPos(" ", intialTop + cursorPos + 1);
+                        moveCursorPos(1, 0, selectOptions.Length);
+                        drawAtPos(">", intialTop + cursorPos + 1);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        drawAtPos(" ", intialTop + cursorPos + 1);
+                        moveCursorPos(-1, 0, selectOptions.Length);
+                        drawAtPos(">", intialTop + cursorPos + 1);
+                        break;
+                    case ConsoleKey.Enter:
+                        int newCursor = intialTop;
+                        for (int i = 0; i <= selectOptions.Length; i++)
+                        {
+                            newCursor++;
+                            Console.SetCursorPosition(0, newCursor);
+                            Console.Write(new string(' ', Console.WindowWidth));
+                        }
+                        Console.SetCursorPosition(0, intialTop);
+                        return cursorPos;
+                }
+            }
         }
     }
 }
