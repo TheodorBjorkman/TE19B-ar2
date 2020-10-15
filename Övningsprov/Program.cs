1e3a1380dd18c6c836dbd9cd35d391ff82fd6cfa
TheodorBjorkman <70270703+TheodorBjorkman@users.noreply.github.com>
2020-10-15 12:12:13 +0200

    a

--- Övningsprov/Program.cs
+++ Övningsprov/Program.cs
@@ -7,14 +7,14 @@ namespace Övningsprov
         static void Main(string[] args)
         {
             int points = 0;
-            while(true)
+            while (true)
             {
                 Console.WriteLine("x*6=42 \nVad är X?");
                 int x;
                 bool succ = int.TryParse(Console.ReadLine(), out x);
-                if(succ)
+                if (succ)
                 {
-                    if(x==7)
+                    if (x == 7)
                     {
                         System.Console.WriteLine("Korrekt!");
                         points++;
@@ -23,7 +23,7 @@ namespace Övningsprov
                     }
                     else
                     {
-                        System.Console.WriteLine($"Nej, {x}*6={x*6}");
+                        System.Console.WriteLine($"Nej, {x}*6={x * 6}");
                         System.Console.WriteLine("Du har " + points + " poäng.");
                         break;
                     }
