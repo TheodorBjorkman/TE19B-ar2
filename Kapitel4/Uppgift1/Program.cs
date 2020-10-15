a1ab720d5a011ea248c4d091ef0095c1d1943203
TheodorBjorkman <70270703+TheodorBjorkman@users.noreply.github.com>
2020-10-15 10:16:55 +0200

    a

---
+++ Kapitel4/Uppgift1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Uppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("skriv nåt");
+            string input = Console.ReadLine();
+            for(int i = input.Length; i >= 0; i--)
+            {
+                
+            }
+        }
+    }
+}
