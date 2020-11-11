c34188c09968450e82bdc3117f70b7ce4c7036d5
TheodorBjorkman <theodor.bjorkman@elev.ga.ntig.se>
2020-11-11 13:52:45 +0100

    a

--- lmao/Program.cs
+++ lmao/Program.cs
@@ -1,38 +1,157 @@
 ﻿using System;
+using TBA;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace lmao
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] )
         {
-            int[] ålder = new int[100];
-            for(int i = 0; i < ålder.Length; i++) ålder[i] = -1;
-            for(int i = 0; i < ålder.Length; i++)
+            int pTotal = 0;
+            Random rnd = new Random();
+            bool play = true;
+            bool draw = true;
+            System.Console.WriteLine("Blackjack!");
+            while (play)
             {
-                bool num = false;
-                while(!num)
+                while (draw)
                 {
-                    string s = Console.ReadLine();
-                    if(s.ToLower() != "n")
+                    int drawed = rnd.Next(1, 11);
+                    pTotal = pTotal + drawed;
+                    System.Console.WriteLine($"Du drog {drawed} du har {pTotal}. Dra ett till kort?");
+                    Selector selector = new Selector();
+                    selector.Add("Dra");
+                    selector.Add("Stanna");
+                    int output = selector.Run();
+                    selector.Clear();
+                    Console.WriteLine();
+                    switch (output)
                     {
-                        num = int.TryParse(s, out ålder[i]);
+                        case 0:
+                            System.Console.WriteLine("Case 1");
+                            break;
+                        case 1:
+                            System.Console.WriteLine("Case 2");
+                            break;
                     }
-                    else
-                    {
-                        break;
-                    }
+                    Environment.Exit(0);
                 }
             }
-            for(int i = 0; i < ålder.Length; i++)
+        }
+    }
+}
+namespace TBA //Big brain kod från Fritiof. Har studerat tillräckligt för att förstå hur den fungerar, att skriva på egen hand skulle dock ta lång tid, mycket google och inte varit lika snyggt.
+{
+    class Selector
+    {
+        int cursorPos = 0;
+        List<string> options;
+
+        public Selector()
+        {
+            options = new List<string>();
+        }
+
+        public void Clear()
+        {
+            options = new List<string>();
+        }
+
+        public void Add(string item)
+        {
+            if (!options.Contains(item))
             {
-                if(ålder[i] > 0)
+                options.Add(item);
+            }
+        }
+
+        public void AddRange(string[] items)
+        {
+            options.AddRange(items);
+        }
+
+        private void moveCursorPos(int amount, int min, int max)
+        {
+            cursorPos = cursorPos + amount;
+            if (cursorPos >= max)
+            {
+                cursorPos = max - 1;
+            }
+            if (cursorPos < min)
+            {
+                cursorPos = min;
+            }
+        }
+
+        void drawAtPos(string text, int pos)
+        {
+            Console.SetCursorPosition(0, pos);
+            Console.Write(text);
+        }
+        static void Write(bool line, string write) //kopierade in från min egen kod. Kan nog göra den till public men jag orkar inte. Gör det också lättare att copy pasta hela mey systemet.
+        {
+
+            if (line == false)
+            {
+                int i;
+                for (i = 0; i <= write.Length - 1; i++)
                 {
-                    System.Console.WriteLine(ålder[i]);
+                    Thread.Sleep(20);
+                    Console.Write(write.Substring(i, 1));
                 }
-                else
+
+            }
+            else
+            {
+                int i;
+                for (i = 0; i <= write.Length - 1; i++)
                 {
-                    break;
+                    Thread.Sleep(20);
+                    Console.Write(write.Substring(i, 1));
+                }
+                System.Console.WriteLine();
+
+            }
+        }
+
+        public int Run()
+        {
+            string[] selectOptions = options.ToArray();
+            int intialTop = Console.CursorTop;
+
+            string printString = "\n";
+            foreach (string option in selectOptions)
+            {
+                printString += "  " + option + "\n";
+            }
+            Write(true, printString);
+            drawAtPos(">", intialTop + cursorPos + 1);
+            while (true)
+            {
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        drawAtPos(" ", intialTop + cursorPos + 1);
+                        moveCursorPos(1, 0, selectOptions.Length);
+                        drawAtPos(">", intialTop + cursorPos + 1);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        drawAtPos(" ", intialTop + cursorPos + 1);
+                        moveCursorPos(-1, 0, selectOptions.Length);
+                        drawAtPos(">", intialTop + cursorPos + 1);
+                        break;
+                    case ConsoleKey.Enter:
+                        int newCursor = intialTop;
+                        for (int i = 0; i <= selectOptions.Length; i++)
+                        {
+                            newCursor++;
+                            Console.SetCursorPosition(0, newCursor);
+                            Console.Write(new string(' ', Console.WindowWidth));
+                        }
+                        Console.SetCursorPosition(0, intialTop);
+                        return cursorPos;
                 }
             }
         }
