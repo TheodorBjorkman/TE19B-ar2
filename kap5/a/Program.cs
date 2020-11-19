3e9945cd5b3614e70ee1b36027a37e11918a47d3
TheodorBjorkman <70270703+TheodorBjorkman@users.noreply.github.com>
2020-11-19 11:12:57 +0100

    a

---
+++ kap5/a/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace a
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = {1, 4, 2, 3, 5};
+            int[] över = new int[array.Length];
+            int[] under = new int[array.Length];
+            int median = 0;
+            for (int i = 0; i < array.Length; i++) 
+            {
+                for (int j = 0; j < array.Length; j++) 
+                {
+                    if (array[i] < array[j]) 
+                    {
+                        över[i]++; 
+                    }
+                    else if (array[i] > array[j]) 
+                    {
+                        under[i]++;
+                    }
+                }
+            }
+            for (int i = 0; i < array.Length; i++) 
+            {
+                if (över[i] < (array.Length / 2) && under[i] < (array.Length / 2))  
+                {
+                    median = array[i];
+                }
+            }
+            System.Console.WriteLine(median);
+        }
+    }
+}
--- kap5/lel/Program.cs
+++ kap5/lel/Program.cs
@@ -14,7 +14,7 @@ namespace lel
             for (int i = 0; i < låst.Length; i++) låst[i] = 0;
             while (!locked) {
             int antalLåst = 0;
-            for (int i = 0; i < kast.Length; i++) kast[i] = rnd.Next(1, 7);
+            for (int i = 0; i < kast.Length; i++) if (låst[i] == 0) kast[i] = rnd.Next(1, 7);
             for (int i = 0; i < kast.Length; i++) if(låst[i] == 0) System.Console.WriteLine($"Kast {(i+1)}: {kast[i]}");
             input = Console.ReadLine().Split(' ');
             for (int i = 0; i < kast.Length; i++) for (int k = 0; k < input.Length; k++) if ((int.Parse(input[k]) - 1) == i) låst[i] = kast[i];
