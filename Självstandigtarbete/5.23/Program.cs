9a6082b723a9162581ba402a6e9546b661661957
TheodorBjorkman <theodor.bjorkman@elev.ga.ntig.se>
2020-11-25 15:04:01 +0100

    a

---
+++ Självstandigtarbete/5.23/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5._23
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] num = { 1, 2, 2, 3, 3, 3, 4, 4, 5, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7 };
+            int[] amount = new int[num.Length];
+            int highestAmount = 0;
+            List<int> most = new List<int>();
+            for (int i = 0; i < num.Length; i++) for (int j = 0; j < num.Length; j++) if (num[i] == num[j]) amount[i]++;
+            for (int i = 0; i < num.Length; i++) if (highestAmount < amount[i]) highestAmount = amount[i];
+            for (int i = 0; i < num.Length; i++) for (int j = 0; j < num.Length; j++) if (amount[i] == highestAmount && !most.Contains(num[i])) most.Add(num[i]);
+            foreach (int number in most) System.Console.WriteLine(number);
+        }
+    }
+}
---
+++ Självstandigtarbete/projektuppgift2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace projektuppgift2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int tableCount = 8;
+            int[] guestCount = new int[tableCount];
+            string[] guestNames = new string[tableCount];
+            System.Console.WriteLine("Detta är Centralrestaurangens bordshanterare");
+            if (File.Exists("tableInfo.txt"))
+            {
+                System.Console.WriteLine("Bordsinformation lästes in från fil");
+            }
+            else
+            {
+                File.WriteLines("tableInfo.txt", "Bord 1 - Inga gäster\nBord 2 - Inga gäster\nBord 3 - Inga gäster\nBord 4 - Inga gäster\nBord 5 - Inga gäster\nBord 6 - Inga gäster\nBord 7 - Namn: Inga gäster\nBord 8 - Inga gäster\nTotalt antal gäster: 0");
+                System.Console.WriteLine("Fil med bordsinformation hittades ej, ny information skapades");
+            }
+        }
+    }
+}
