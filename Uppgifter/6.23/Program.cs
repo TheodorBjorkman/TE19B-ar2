acd19512acec5bca1ec25da64dfeb1849e126194
TheodorBjorkman <theodor.bjorkman@elev.ga.ntig.se>
2021-04-14 14:44:40 +0200

    6.23 plus lite till

---
+++ Uppgifter/6.23/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _6._23
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                Console.WriteLine(EverySecondWordCapitalized(input));
+                Console.WriteLine(GörFörstaBokstavenStor(input));
+            }
+        }
+        static string EverySecondWordCapitalized(string input)
+        {
+            string[] inputArray = input.Split(' ');
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    inputArray[i] = inputArray[i].ToUpper();
+                }
+                else
+                {
+                    inputArray[i] = inputArray[i].ToLower();
+                }
+            }
+            return String.Join(' ', inputArray);
+        }
+        static string GörFörstaBokstavenStor(string input)
+        {
+            string[] inputArray = input.Split(' ');
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                inputArray[i] = inputArray[i].Substring(0, 1).ToUpper() + inputArray[i].Substring(1);
+            }
+            return String.Join(' ', inputArray);
+        }
+    }
+}
---
+++ Uppgifter/Sänkaskepp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sänkaskepp
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
