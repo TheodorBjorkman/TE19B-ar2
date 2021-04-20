a246e44177b24dff7e8b5dbd1e4622c884edff20
TheodorBjorkman <70270703+TheodorBjorkman@users.noreply.github.com>
2021-04-20 13:44:54 +0200

    asdf

---
+++ Things/6.10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _6._10
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(TryInt());
+        }
+        static bool TryInt()
+        {
+            int a = 0;
+            System.Console.WriteLine("Enter input");
+            return int.TryParse(Console.ReadLine().Trim(), out a);
+        }
+    }
+}
---
+++ Things/6.9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _6._9
+{
+    class Program
+    {
+        static void Main()
+        {
+            System.Console.WriteLine(ReadDouble());
+        }
+
+        static double ReadDouble()
+        {
+            double a = 0;
+            System.Console.WriteLine("Input a number");
+            while (!double.TryParse(Console.ReadLine(), out a))
+            {
+                System.Console.WriteLine("Input a number (decimals are seperated by ',')");
+            }
+            return a;
+        }
+    }
+}
